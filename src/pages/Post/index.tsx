469602ee4489deb375b6ffd439bff0c56e984f29
Aris Nur <ariezncahyo@gmail.com>
2023-04-18 23:41:12 +0700

    consume create post get page

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,20 +1,28 @@
 import Layout from "@/components/Layout";
 import type { ReactElement } from 'react';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { AiOutlineSearch, AiOutlineFileImage, AiOutlinePlus} from 'react-icons/ai';
-import { ButtonSubmit } from "@/components/Button/ButtonSubmit";
-import { authActions } from "@/store/auth/slices";
+import { AiOutlineSearch, AiOutlineFileImage, AiOutlinePlus, AiOutlineHeart} from 'react-icons/ai';
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { ButtonOk } from "@/components/Button";
 import { ModalConfirm, ModalPost } from "@/components/Modal";
-import { FcLike } from 'react-icons/fc';
 import { Pagination } from "@/components/Pagination";
+import { postActions } from "@/store/post/slices";
 
 export default function Post(): ReactElement {
   const [showConfirmDelete, setShowConfirmDelete] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const { loading }: any = useAppSelector(state=>state.loading);
+  const { post }: any = useAppSelector(state=>state.post);
+  const [ filter, setFilter ] = useState<any>({});
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(postActions.getPost(filter));
+  }, [filter]);
+
 
   const onDelete = () => {
 
@@ -52,26 +60,33 @@ export default function Post(): ReactElement {
                   className="rounded-lg border-transparent flex-1 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 text-base focus:outline-none"
                   type="text"
                   placeholder="Search tags or caption"
-                  // {...register('username', { required: true })}
+                  onChange={(e)=> setFilter({
+                    ...filter,
+                    search: e.target.value
+                  })}
                 />
               </div>
 
               <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {
-                  [...Array.from(Array(10).keys())].map((item, index) => {
+                  post?.data?.rows && post?.data?.rows.map((item: any, index: any) => {
                     return (
                       <div key={index} className="w-full border rounded-lg min-h-64 hover:bg-gray-200">
                         <div className="flex w-full h-48 place-content-center justify-center items-center border-b-2">
-                          <AiOutlineFileImage className="text-2xl"/>
+                          {
+                            item?.image ? (
+                              <img src={item?.image}/>
+                            ) : (<AiOutlineFileImage className="text-2xl"/>)
+                          }
                         </div>
                         <div className="flex flex-col p-1">
                           <div className="flex gap-2 items-center">
-                            <FcLike/><span className="text-sm">300</span>
+                            <AiOutlineHeart className="text-2xl cursor-pointer"/><span className="text-sm">{item?.likes}</span>
                           </div>
                           <div className="flex gap-2 w-full">
                             <div className="flex flex-col w-full">
-                              <div className="text-sm font-bold">username</div>
-                              <div className="text-sm">Senja di malam takbir menyambut gema jiwa-jiwa</div>
+                              <div className="text-sm font-bold">{item?.user?.username}</div>
+                              <div className="text-sm">{item?.caption}</div>
                             </div>
                             <div className="flex flex-col gap-1">
                               <div className="text-xs bg-red-400 p-0 px-1 rounded-md text-white text-center hover:bg-red-700 cursor-pointer"
@@ -83,7 +98,7 @@ export default function Post(): ReactElement {
                             </div>
                           </div>
                           <div className="flex text-xs my-4">
-                            <p>#sunset #senja #surabaya</p>
+                            <p>{item?.tags}</p>
                           </div>
                         </div>
                       </div>
